3daa666bf5ca80732abbf3b41a78308c3d8d8006
paudez14 <paudpez@hotmail.com>
2021-06-20 00:59:02 -0500

    Actualización del Archivo Principal

--- proyecto.asm
+++ proyecto.asm
@@ -5,12 +5,14 @@ titulo2: .asciiz "Lista de Productos Disponibles"
 titulo3: .asciiz "Total a Pagar:  "
 titulo4: .asciiz "Gracias por su Compra."
 titulo5: .asciiz "Opción Incorrecta. Ingrese de nuevo"
+titulo6: .asciiz "Ingrese el codigo del producto:  "
+titulo7: .asciiz "Ingrese la cantidad a comprar:  "
+titulo8: .asciiz "¿Desea continuar? Si[1] No[2]:  "
 newLine: .asciiz "\n"
 texto: .space len
 .text
-
-.globl proyecto
-proyecto:
+.globl main
+main:
 	#presenta Bienvenido a MiniMarket ESPOL
 	li $v0, 4
     	la $a0, titulo
@@ -34,7 +36,48 @@ proyecto:
     	li $v0, 4
     	la $a0, newLine
     	syscall
-    	li $v0, 4
+    	j salir
+compras:
+	addi $sp,$sp,-4
+   	sw $ra,($sp)
+   	addi $t0, $zero, 1
+   	validacion:
+	   	beq $t0, 2, salida
+		#presenta Ingrese el codigo del producto:  
+    		li $v0, 4
+    		la $a0, titulo6
+    		syscall
+    		li $v0,5
+    		la $a0, texto
+    		li $a1, len
+    		syscall
+    		#presenta Ingrese la cantidad a comprar:  
+    		li $v0, 4
+    		la $a0, titulo7
+    		syscall
+    		li $v0,5
+    		la $a0, texto
+    		li $a1, len
+    		syscall
+    		#Despues que se muestre este mensaje ya debería calcular el pago
+    		#presenta ¿Desea continuar? Si[1] No[2]:  
+    		#Validado el 1 y el  2	
+    		li $v0, 4
+    		la $a0, titulo8
+    		syscall
+    		li $v0,5
+    		la $a0, texto
+    		li $a1, len
+    		syscall
+    		move $t0, $v0
+    		j validacion    		
+    	salida: 
+    		#sale de la funcion
+    		lw $ra, ($sp)
+    		addi $sp,$sp,4
+    		jr $ra 
+salir:
+	#Sale del programa
+	li $v0, 4
     	la $a0, titulo4
     	syscall
-	jal compras
